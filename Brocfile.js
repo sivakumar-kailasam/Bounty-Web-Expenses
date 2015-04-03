e1f8dfc058070ec51e3b233e815d3bbac942b938
Sivakumar Kailasam <sivakumar.ur.friend@gmail.com>
2015-04-03 18:03:05 +0530

    include c3

--- Brocfile.js
+++ Brocfile.js
@@ -4,12 +4,32 @@
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 var app = new EmberApp({
-        storeConfigInMeta: false
+    storeConfigInMeta: false
 });
 
 var isProductionBuild = (app.env === 'production');
 var pickFiles = require('broccoli-static-compiler');
 var mergeTrees = require('broccoli-merge-trees');
+
+/* C3 js */
+app.import({
+    development: 'bower_components/c3/c3.css',
+    production: 'bower_components/c3/c3.min.css'
+});
+
+
+app.import({
+    development: 'bower_components/d3/d3.js',
+    production: 'bower_components/d3/d3.min.js'
+});
+
+
+app.import({
+    development: 'bower_components/c3/c3.js',
+    production: 'bower_components/c3/c3.min.js'
+});
+
+
 
 /* Animate.css */
 app.import('bower_components/animate.css/animate.css');
@@ -17,18 +37,18 @@ app.import('bower_components/animate.css/animate.css');
 
 /* materializer assets */
 app.import({
-        development: 'bower_components/materialize/dist/css/materialize.css',
-        production: 'bower_components/materialize/dist/css/materialize.min.css'
+    development: 'bower_components/materialize/dist/css/materialize.css',
+    production: 'bower_components/materialize/dist/css/materialize.min.css'
 });
 app.import({
-        development: 'bower_components/materialize/dist/js/materialize.js',
-        production: 'bower_components/materialize/dist/js/materialize.min.js'
+    development: 'bower_components/materialize/dist/js/materialize.js',
+    production: 'bower_components/materialize/dist/js/materialize.min.js'
 });
 
 var materializeFontFiles = pickFiles('bower_components/materialize/dist/font', {
-        srcDir: '/',
-        files: ['**/*.woff', '**/*.eot', '**/*.svg', '**/*.ttf'],
-        destDir: '/font'
+    srcDir: '/',
+    files: ['**/*.woff', '**/*.eot', '**/*.svg', '**/*.ttf'],
+    destDir: '/font'
 });
 
 
